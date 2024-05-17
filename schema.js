f7ec7cb9f6a58b7eef301ad28181e2ae03bdd1c5
yashg019 <goswamiyash019@gmail.com>
2024-05-17 11:34:23 +0530

    first commit

---
+++ schema.js
@@ -0,0 +1,14 @@
+const Joi = require("joi");
+
+//Schema for listings
+module.exports.listingSchema = Joi.object({ 
+    listing: Joi.object({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        location: Joi.string().required(),
+        country: Joi.string().required(),
+        price: Joi.number().required(),
+        image: Joi.string().allow("", null),
+    }).required(),
+});
+
